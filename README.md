9ae364d6220a5757761b9b92db0d97acf89ab953
Ramazan APAYDIN <apaydin541@gmail.com>
2020-12-10 01:12:05 +0300

    Inıt

---
+++ README.md
@@ -0,0 +1,33 @@
+## Webpack Theme Boilerplate
+
+>This webpack template is designed for theme development.
+
+
+### Features
+* Babel ES6 Transpiler
+* Saas (scss) Loader
+* PostCss Loader 
+* Css Minimizer
+* Css Autoprefixer  
+* Image Loader (png, jpg, gif, svg)
+* Font Loader (woff, woff2, ttf, otf, eot)
+* Eslint JS Linter
+
+---
+
+#### Install
+```bash
+git clone https://github.com/appaydin/webpack-boilerplate template
+yarn install
+```
+
+#### Development
+```bash
+yarn dev
+yarn watch 
+```
+
+#### Production
+```bash
+yarn build
+```
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: './src/js/app.js',
+    output: {
+        path: path.resolve(__dirname, 'public/dist'),
+        filename: 'app.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env']
+                        }
+                    },
+                    {
+                        loader: 'eslint-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: "css-loader",
+                    },
+                    {
+                        loader: "postcss-loader"
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            implementation: require("sass")
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            outputPath: 'images'
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(woff|woff2|ttf|otf|eot)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            outputPath: 'fonts'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "app.css"
+        })
+    ]
+};
